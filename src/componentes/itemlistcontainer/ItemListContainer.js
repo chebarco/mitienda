77a9cd4d934c709a56258672226a00aefc8ba6ff
Chechu Barco <86536881+chebarco@users.noreply.github.com>
2021-12-27 12:12:19 -0300

    item/firestore
    lo trae desde firestorebase

--- src/componentes/itemlistcontainer/ItemListContainer.js
+++ src/componentes/itemlistcontainer/ItemListContainer.js
@@ -1,10 +1,11 @@
 import {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
-import { getFetch } from "../../helpers/getFetch"
 import ItemList from "../ItemList/ItemList"
+import {collection, doc, getDoc, getFirestore, getDocs} from "firebase/firestore"
 
 function ItemListContainer( {greeting} ) {
     const [productos, setProductos] = useState([])
+    const [producto, setProducto] = useState({})
     const [loading, setLoading] = useState(true)
 
 
@@ -14,38 +15,30 @@ function ItemListContainer( {greeting} ) {
     const { idCate } = useParams() 
     
     
-    useEffect(() => {
-        if (idCate) {
-            getFetch
-            .then(resp => setProductos(resp.filter(prod => prod.categoria === idCate))) 
-            .catch(err => console.log(err))
-            .finally(()=>setLoading(false))            
-        } else {
-            getFetch
-            .then(resp => setProductos(resp)) 
-            .catch(err => console.log(err))
-            .finally(()=>setLoading(false))               
-        }
-    }, [idCate]) 
+    //useEffect(() => {
+       // if (idCate) {
+       //     getFetch
+       //     .then(resp => setProductos(resp.filter(prod => prod.categoria === idCate))) 
+       //     .catch(err => console.log(err))
+       //     .finally(()=>setLoading(false))            
+       // } else {
+       //     getFetch
+        //    .then(resp => setProductos(resp)) 
+        //    .catch(err => console.log(err))
+       //     .finally(()=>setLoading(false))               
+       // }
+    //}, [idCate]) 
     
     
-   //ejemplo de evento
-   const handleClick=(e)=>{
-        e.preventDefault() 
-        setBool(!bool)
-    }
-
-    const handleAgregar=()=>{
-        setProductos([
-            ...productos,
-            { id: "8", name: "Gorra 7", url: 'https://www.remerasya.com/pub/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/r/e/remera_negra_lisa.jpg', categoria: "remera" , price: 2 }
-        ])
-    }
+useEffect(() =>{
+    const db = getFirestore ()
+    const queryCollection = collection (db, "item")
+    getDocs(queryCollection)
+    .then(resp => setProductos(resp.docs.map( prod=> ({id: prod.id, ...prod.data()}))))
+    .finally(()=> setLoading(false))
+}, [])
     
-    console.log('itemListContainer')
-    console.log(productos)
-    console.log(bool)
-    // [] => retorna otro array [<li>1</li>,....]
+    
     
     return (
         <div>
--- src/index.js
+++ src/index.js
@@ -3,19 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { getFirestoreApp } from './config/getFirestoreApp'
 
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCST3RTRbZ25u1xmdqLC2PRP5Y67z_7UE4",
-  authDomain: "tiendadechocolates-22a80.firebaseapp.com",
-  projectId: "tiendadechocolates-22a80",
-  storageBucket: "tiendadechocolates-22a80.appspot.com",
-  messagingSenderId: "917028776747",
-  appId: "1:917028776747:web:7fdaefa4b709a01f76736e"
-};
-
-const app = initializeApp(firebaseConfig);
+getFirestoreApp()
 
 
 ReactDOM.render(
